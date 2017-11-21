0d11fa68f69363e6f9e74b245388a5b964a40ee0
bernardarzur <33778258+bernardarzur@users.noreply.github.com>
2017-11-21 14:10:38 +0100

    Add files via upload

---
+++ main_B.py
@@ -0,0 +1,27 @@
+
+import pycom
+from network import LoRa
+import socket
+import time
+
+lora = LoRa(mode=LoRa.LORA, frequency=863000000)
+s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
+s.setblocking(False)
+
+pycom.heartbeat(False)
+pycom.rgbled(0xaa5500)
+print("jaune")    
+time.sleep(2)
+ping=('ping'+str(2**24))*10
+while True:
+    s.send(ping)
+    pycom.rgbled(0xff0000)
+    print("paquet envoyé",  ping)
+    time.sleep(.2)
+    pycom.rgbled(0x000000)         
+    time.sleep(4)
+    
+    
+    
+    
+
