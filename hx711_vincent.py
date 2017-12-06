39849adff946ccabeb3e3a5c4b0c5c9723333b15
Vincent MESSIÉ <vincent.messie@gmail.com>
2017-12-06 22:39:19 +0100

    fix last bug I hope

--- hx711_vincent.py
+++ hx711_vincent.py
@@ -71,7 +71,7 @@ class HX711:
         readbits = ""
 
 
-        return struct.unpack('<i', measure + ('\0' if measure[2] < 128 else '\xff'))
+        return struct.unpack('<i', dataBits + ('\0' if dataBits[2] < 128 else '\xff'))
 
     def read_average(self, times=3):
         sum = 0
